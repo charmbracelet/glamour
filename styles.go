fa41f6a15fbe3392564235d86eb178fb4594c4b5
Tom Payne <twpayne@gmail.com>
2020-01-05 18:51:22 +0100

    Add ASCII default style

--- styles.go
+++ styles.go
@@ -7,6 +7,113 @@ import (
 )
 
 var (
+	// ASCIIStyleConfig uses only ASCII characters.
+	ASCIIStyleConfig = ansi.StyleConfig{
+		Document: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				BlockPrefix: "\n",
+				BlockSuffix: "\n",
+			},
+			Indent: uintPtr(2),
+		},
+		BlockQuote: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{},
+			Indent:         uintPtr(1),
+			IndentToken:    stringPtr("| "),
+		},
+		Paragraph: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{},
+		},
+		List: ansi.StyleList{
+			StyleBlock: ansi.StyleBlock{
+				StylePrimitive: ansi.StylePrimitive{},
+			},
+			LevelIndent: 4,
+		},
+		Heading: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				BlockSuffix: "\n",
+			},
+		},
+		H1: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix: "# ",
+			},
+		},
+		H2: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix: "## ",
+			},
+		},
+		H3: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix: "### ",
+			},
+		},
+		H4: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix: "#### ",
+			},
+		},
+		H5: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix: "##### ",
+			},
+		},
+		H6: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix: "###### ",
+			},
+		},
+		Strikethrough: ansi.StylePrimitive{
+			BlockPrefix: "~~",
+			BlockSuffix: "~~",
+		},
+		Emph: ansi.StylePrimitive{
+			BlockPrefix: "*",
+			BlockSuffix: "*",
+		},
+		Strong: ansi.StylePrimitive{
+			BlockPrefix: "**",
+			BlockSuffix: "**",
+		},
+		HorizontalRule: ansi.StylePrimitive{
+			Format: "\n--------\n",
+		},
+		Item: ansi.StylePrimitive{
+			BlockPrefix: "• ",
+		},
+		Enumeration: ansi.StylePrimitive{
+			BlockPrefix: ". ",
+		},
+		Task: ansi.StyleTask{
+			Ticked:   "[x] ",
+			Unticked: "[ ] ",
+		},
+		ImageText: ansi.StylePrimitive{
+			Format: "Image: {{.text}} →",
+		},
+		Code: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				BlockPrefix: "`",
+				BlockSuffix: "`",
+			},
+		},
+		CodeBlock: ansi.StyleCodeBlock{
+			StyleBlock: ansi.StyleBlock{
+				Margin: uintPtr(2),
+			},
+		},
+		Table: ansi.StyleTable{
+			CenterSeparator: stringPtr("+"),
+			ColumnSeparator: stringPtr("|"),
+			RowSeparator:    stringPtr("-"),
+		},
+		DefinitionDescription: ansi.StylePrimitive{
+			BlockPrefix: "\n* ",
+		},
+	}
+
 	// DarkStyleConfig is the default dark style.
 	DarkStyleConfig = ansi.StyleConfig{
 		Document: ansi.StyleBlock{
@@ -626,6 +733,7 @@ var (
 
 	// DefaultStyles are the default styles.
 	DefaultStyles = map[string]*ansi.StyleConfig{
+		"ascii":  &ASCIIStyleConfig,
 		"dark":   &DarkStyleConfig,
 		"light":  &LightStyleConfig,
 		"notty":  &NoTTYStyleConfig,
