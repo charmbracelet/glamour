5a2216e688ed5b65d0686bfe48989187a11ae8ed
Ayman Bagabas <ayman.bagabas@gmail.com>
2022-10-17 12:31:23 -0400

    fix(ansi): don't register chroma styles on every render
    * determine chroma terminal formatter from termenv.Profile
    
    no need to register chroma themes on every codeblock render

--- ansi/codeblock.go
+++ ansi/codeblock.go
@@ -2,17 +2,10 @@ package ansi
 
 import (
 	"io"
-	"sync"
 
 	"github.com/alecthomas/chroma/v2/quick"
 	"github.com/muesli/reflow/indent"
 	"github.com/muesli/termenv"
-)
-
-var (
-	// mutex for synchronizing access to the chroma style registry.
-	// Related https://github.com/alecthomas/chroma/pull/650
-	mutex = sync.Mutex{}
 )
 
 // A CodeBlockElement is used to render code blocks.
@@ -27,6 +20,8 @@ func (e *CodeBlockElement) Render(w io.Writer, ctx RenderContext) error {
 	var indentation uint
 	var margin uint
 	rules := ctx.options.Styles.CodeBlock
+	theme := rules.Theme
+
 	if rules.Indent != nil {
 		indentation = *rules.Indent
 	}
@@ -34,21 +29,22 @@ func (e *CodeBlockElement) Render(w io.Writer, ctx RenderContext) error {
 		margin = *rules.Margin
 	}
 
-	// Don't register the style if it's already registered.
-	if rules.Chroma != nil && ctx.options.ColorProfile != termenv.Ascii {
-		mutex.Lock()
-		ChromaRegister(&ctx.options.Styles)
-		mutex.Unlock()
-	}
-	theme := rules.Theme
-
 	iw := indent.NewWriterPipe(w, indentation+margin, func(wr io.Writer) {
 		renderText(w, ctx.options.ColorProfile, bs.Current().Style.StylePrimitive, " ")
 	})
 
 	if len(theme) > 0 {
+		var formatter string
+		switch ctx.options.ColorProfile {
+		case termenv.TrueColor, termenv.ANSI256:
+			formatter = "terminal256"
+		case termenv.ANSI:
+			formatter = "terminal16"
+		default:
+			formatter = "terminal"
+		}
 		renderText(iw, ctx.options.ColorProfile, bs.Current().Style.StylePrimitive, rules.BlockPrefix)
-		err := quick.Highlight(iw, e.Code, e.Language, "terminal256", theme)
+		err := quick.Highlight(iw, e.Code, e.Language, formatter, theme)
 		if err != nil {
 			return err
 		}
