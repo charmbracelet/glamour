39d8a9f8724499696b3de6c1e49da5f5c82934e0
Dieter Eickstaedt <eickstaedt@deicon.de>
2022-11-17 22:23:21 +0100

    feature: Option to toggle between Link Text only and Link Text + URL Rendering

--- examples/custom_renderer/main.go
+++ examples/custom_renderer/main.go
@@ -10,11 +10,13 @@ func main() {
 	in := `# Custom Renderer
 
 Word-wrapping will occur when lines exceed the limit of 40 characters.
+[Hello World](http://www.google.de)
 `
 
 	r, _ := glamour.NewTermRenderer(
 		glamour.WithStandardStyle("dark"),
 		glamour.WithWordWrap(40),
+		glamour.WithLinkTextOnly(true),
 	)
 
 	out, _ := r.Render(in)
--- glamour.go
+++ glamour.go
@@ -71,6 +71,7 @@ func NewTermRenderer(options ...TermRendererOption) (*TermRenderer, error) {
 		ansiOptions: ansi.Options{
 			WordWrap:     80,
 			ColorProfile: termenv.TrueColor,
+			LinkTextOnly: false,
 		},
 	}
 	for _, o := range options {
@@ -181,6 +182,13 @@ func WithStylesFromJSONFile(filename string) TermRendererOption {
 func WithWordWrap(wordWrap int) TermRendererOption {
 	return func(tr *TermRenderer) error {
 		tr.ansiOptions.WordWrap = wordWrap
+		return nil
+	}
+}
+
+func WithLinkTextOnly(onlyLinkText bool) TermRendererOption {
+	return func(tr *TermRenderer) error {
+		tr.ansiOptions.LinkTextOnly = onlyLinkText
 		return nil
 	}
 }
