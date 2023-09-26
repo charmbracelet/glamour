ce3bbe8b0db7a4c62544e377e7fb527a27dff032
bashbunni <bunni@bashbunni.dev>
2023-09-26 11:41:45 -0700

    feat: render table with lipgloss

--- ansi/table.go
+++ ansi/table.go
@@ -1,27 +1,26 @@
 package ansi
 
 import (
+	"fmt"
 	"io"
 
-	"github.com/muesli/reflow/indent"
-	"github.com/olekukonko/tablewriter"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
 )
 
 // A TableElement is used to render tables.
 type TableElement struct {
-	writer      *tablewriter.Table
+	lipgloss    *table.Table
 	styleWriter *StyleWriter
-	header      []string
-	cell        []string
+	headers     []string
+	row        []string
 }
 
 // A TableRowElement is used to render a single row in a table.
-type TableRowElement struct {
-}
+type TableRowElement struct{}
 
 // A TableHeadElement is used to render a table's head element.
-type TableHeadElement struct {
-}
+type TableHeadElement struct{}
 
 // A TableCellElement is used to render a single cell in a row.
 type TableCellElement struct {
@@ -32,45 +31,40 @@ type TableCellElement struct {
 func (e *TableElement) Render(w io.Writer, ctx RenderContext) error {
 	bs := ctx.blockStack
 
-	var indentation uint
-	var margin uint
+	// TODO add indentation and margin for the table
+	// 	var indentation uint
+	// 	var margin uint
 	rules := ctx.options.Styles.Table
-	if rules.Indent != nil {
-		indentation = *rules.Indent
-	}
-	if rules.Margin != nil {
-		margin = *rules.Margin
-	}
+	// 	if rules.Indent != nil {
+	// 		indentation = *rules.Indent
+	// 	}
+	// 	if rules.Margin != nil {
+	// 		margin = *rules.Margin
+	// 	}
 
-	iw := indent.NewWriterPipe(w, indentation+margin, func(wr io.Writer) {
-		renderText(w, ctx.options.ColorProfile, bs.Current().Style.StylePrimitive, " ")
-	})
+	// iw := indent.NewWriterPipe(w, indentation+margin, func(wr io.Writer) {
+	// 	renderText(w, ctx.options.ColorProfile, bs.Current().Style.StylePrimitive, " ")
+	// })
 
 	style := bs.With(rules.StylePrimitive)
-	ctx.table.styleWriter = NewStyleWriter(ctx, iw, style)
+	ctx.table.styleWriter = NewStyleWriter(ctx, w, style)
 
 	renderText(w, ctx.options.ColorProfile, bs.Current().Style.StylePrimitive, rules.BlockPrefix)
-	renderText(ctx.table.styleWriter, ctx.options.ColorProfile, style, rules.Prefix)
-	ctx.table.writer = tablewriter.NewWriter(ctx.table.styleWriter)
+	renderText(w, ctx.options.ColorProfile, style, rules.Prefix)
+	// 	ctx.table.writer = tablewriter.NewWriter(ctx.table.styleWriter)
+	ctx.table.lipgloss = table.New()
 	return nil
 }
 
 func (e *TableElement) Finish(w io.Writer, ctx RenderContext) error {
 	rules := ctx.options.Styles.Table
 
-	ctx.table.writer.SetBorders(tablewriter.Border{Left: false, Top: false, Right: false, Bottom: false})
-	if rules.CenterSeparator != nil {
-		ctx.table.writer.SetCenterSeparator(*rules.CenterSeparator)
-	}
-	if rules.ColumnSeparator != nil {
-		ctx.table.writer.SetColumnSeparator(*rules.ColumnSeparator)
-	}
-	if rules.RowSeparator != nil {
-		ctx.table.writer.SetRowSeparator(*rules.RowSeparator)
-	}
+	ctx.table.lipgloss.Border(lipgloss.NormalBorder())
 
-	ctx.table.writer.Render()
-	ctx.table.writer = nil
+	// TODO remove styleWriter dep; not needed with lipgloss
+	ctx.table.styleWriter.Write([]byte(ctx.table.lipgloss.Render()))
+
+	ctx.table.lipgloss = nil
 
 	renderText(ctx.table.styleWriter, ctx.options.ColorProfile, ctx.blockStack.With(rules.StylePrimitive), rules.Suffix)
 	renderText(ctx.table.styleWriter, ctx.options.ColorProfile, ctx.blockStack.Current().Style.StylePrimitive, rules.BlockSuffix)
@@ -78,30 +72,52 @@ func (e *TableElement) Finish(w io.Writer, ctx RenderContext) error {
 }
 
 func (e *TableRowElement) Finish(w io.Writer, ctx RenderContext) error {
-	if ctx.table.writer == nil {
+	if ctx.table.lipgloss == nil {
 		return nil
 	}
+	if len(ctx.table.row) == 0 {
+		panic(fmt.Sprintf("got an empty row %#v", ctx.table.row))
+	}
+	ctx.table.lipgloss.Row(StringToAny(ctx.table.row)...)
 
-	ctx.table.writer.Append(ctx.table.cell)
-	ctx.table.cell = []string{}
+	// Append the current cell to our current row?
+	// Maybe we should just write to TableElement, then render our final table
+	// given the data in TableElement ctx.table.writer.Append(ctx.table.cell)
+
+	// reset working row
+	ctx.table.row = []string{}
 	return nil
 }
 
 func (e *TableHeadElement) Finish(w io.Writer, ctx RenderContext) error {
-	if ctx.table.writer == nil {
+	if ctx.table.lipgloss == nil {
 		return nil
 	}
 
-	ctx.table.writer.SetHeader(ctx.table.header)
-	ctx.table.header = []string{}
+	headers := StringToAny(ctx.table.headers)
+	ctx.table.lipgloss.Headers(headers...)
+	ctx.table.headers = []string{}
 	return nil
 }
 
+// StringToAny returns the headers as generic types for the lipgloss table.
+func StringToAny(s []string) []any {
+	out := make([]any, len(s))
+	for i, str := range s {
+		out[i] = str
+	}
+	fmt.Print(s)
+
+	fmt.Print(out)
+	return out
+}
+
+// TODO apply individual cell styling here if desired
 func (e *TableCellElement) Render(w io.Writer, ctx RenderContext) error {
 	if e.Head {
-		ctx.table.header = append(ctx.table.header, e.Text)
+		ctx.table.headers = append(ctx.table.headers, e.Text)
 	} else {
-		ctx.table.cell = append(ctx.table.cell, e.Text)
+		ctx.table.row = append(ctx.table.row, e.Text)
 	}
 
 	return nil
--- go.mod
+++ go.mod
@@ -1,13 +1,13 @@
 module github.com/charmbracelet/glamour
 
-go 1.17
+go 1.18
 
 require (
 	github.com/alecthomas/chroma/v2 v2.8.0
+	github.com/charmbracelet/lipgloss v0.8.0
 	github.com/microcosm-cc/bluemonday v1.0.25
 	github.com/muesli/reflow v0.3.0
 	github.com/muesli/termenv v0.15.2
-	github.com/olekukonko/tablewriter v0.0.5
 	github.com/yuin/goldmark v1.5.4
 	github.com/yuin/goldmark-emoji v1.0.2
 )
@@ -21,6 +21,9 @@ require (
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.12.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
+
+replace github.com/charmbracelet/lipgloss => ../lipgloss
