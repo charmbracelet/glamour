c3e91406ef1f6340728b8673e397bd73a649dfd2
Xinhe Wang <wangxinhe06@gmail.com>
2024-07-31 03:07:28 +0800

    feat(auto): auto NoTTY style (#268)
    Co-authored-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- glamour.go
+++ glamour.go
@@ -13,6 +13,7 @@ import (
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/renderer"
 	"github.com/yuin/goldmark/util"
+	"golang.org/x/term"
 
 	"github.com/charmbracelet/glamour/ansi"
 )
@@ -260,6 +261,9 @@ func getEnvironmentStyle() string {
 
 func getDefaultStyle(style string) (*ansi.StyleConfig, error) {
 	if style == AutoStyle {
+		if !term.IsTerminal(int(os.Stdout.Fd())) {
+			return &NoTTYStyleConfig, nil
+		}
 		if termenv.HasDarkBackground() {
 			return &DarkStyleConfig, nil
 		}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/charmbracelet/x/exp/golden v0.0.0-20240715153702-9ba8adf781c4
 	github.com/microcosm-cc/bluemonday v1.0.27
 	github.com/muesli/reflow v0.3.0
+	golang.org/x/term v0.10.0
 	github.com/muesli/termenv v0.15.3-0.20240618155329-98d742f6907a
 	github.com/yuin/goldmark v1.7.4
 	github.com/yuin/goldmark-emoji v1.0.3
