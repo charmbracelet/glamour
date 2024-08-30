charmbracelet/glamour

commit 5f5965ed1282e3d4299ca9eac221771fc2488159

fix: many rendering issues (#317)
* wip

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

* wip

* wip

* wip

* fix: autolink

closes #290

* fix: escape characters

closes #106
closes #274
closes #311

* fix: table

* ci: golangci lint update

* feat: use x/golden

* test: #106

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

* test: #290

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

* test: #312

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

* test: #257

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

* test: #149

* fix: #239

* feat: use lipgloss table

closes #262

Co-authored-by: bashbunni <bunni@bashbunni.dev>

* fix: codespan is not a block

* test: #315

* test: #316

* fix: #316

* test: table

* fix: codespans, tables

* test: table

* test: #117

* test: #60

* fix: rm stylewriter

* fix: #313

* fix: margin

* fix: blocks and word wrap

* fix: build

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

* chore: gitattributes

* fix: test opt

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

* fix: stable lipgloss

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

* fix: double styles

* fix: tables

* fix: tables

---------

Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
Co-authored-by: bashbunni <bunni@bashbunni.dev>

ansi/blockstack.go
@@ -59,10 +59,10 @@ func (s BlockStack) Margin() uint {
 
 // Width returns the available rendering width.
 func (s BlockStack) Width(ctx RenderContext) uint {
-	if s.Indent()+s.Margin()*2 > uint(ctx.options.WordWrap) {
+	if s.Indent()*s.Margin() > uint(ctx.options.WordWrap) {
 		return 0
 	}
-	return uint(ctx.options.WordWrap) - s.Indent() - s.Margin()*2
+	return uint(ctx.options.WordWrap) - s.Indent()*s.Margin()
 }
 
 // Parent returns the current BlockElement's parent.

commit bf73daf96e6894c14aa32261ad7fdb1793562c74

fix: broken margin rendering
The margin and indent are both absolute values and should not be
multiplied with one another. This commit partially reverts 5f5965e which
introduced the bug.

Fixes: charmbracelet/glamour#331

ansi/blockstack.go
@@ -59,10 +59,10 @@ func (s BlockStack) Margin() uint {
 
 // Width returns the available rendering width.
 func (s BlockStack) Width(ctx RenderContext) uint {
-	if s.Indent()*s.Margin() > uint(ctx.options.WordWrap) {
+	if s.Indent()+s.Margin()*2 > uint(ctx.options.WordWrap) {
 		return 0
 	}
-	return uint(ctx.options.WordWrap) - s.Indent()*s.Margin()
+	return uint(ctx.options.WordWrap) - s.Indent() - s.Margin()*2
 }
 
 // Parent returns the current BlockElement's parent.