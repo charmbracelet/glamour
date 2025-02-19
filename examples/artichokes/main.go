12845f8d9395398a4a08a0c00f607cc1a5b3125d
Christian Rocha <christian@rocha.is>
2025-02-19 06:42:27 -0500

    docs(example): update example to demonstrate color downsampling

--- examples/artichokes/main.go
+++ examples/artichokes/main.go
@@ -1,45 +1,57 @@
 package main
 
+// This example illustrates how to render markdown and downsample colors when
+// necessary per the detected color profile of the terminal.
+
 import (
+	"bytes"
 	"embed"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/charmbracelet/colorprofile"
 	"github.com/charmbracelet/glamour"
-	"github.com/muesli/termenv"
 )
 
 //go:embed artichokes.md
 var f embed.FS
 
 func main() {
-	// Provide a style by name (optional)
-	var style string
-	if len(os.Args) < 2 {
-		// check env, if unset then use default style
-		style = os.Getenv("GLAMOUR_STYLE")
-	} else {
-		style = os.Args[1]
+	// Open the file to learn a thing or two about artichokes.
+	f, err := f.Open("artichokes.md")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file: %s\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	// Read the data.
+	var buf bytes.Buffer
+	if _, readErr := buf.ReadFrom(f); readErr != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %s\n", readErr)
+		os.Exit(1)
 	}
 
-	// Let's learn a 'lil something about artichokes...
-	b, err := f.ReadFile("artichokes.md")
+	// Create a new colorprofile writer. We'll use it to detect the color
+	// profile and downsample colors when necessary.
+	w := colorprofile.NewWriter(os.Stdout, os.Environ())
+
+	// While we're at it, let's jot down the detected color profile in the
+	// markdown output while we're at it.
+	fmt.Fprintf(&buf, "\n\nBy the way, this was rendererd as _%s._\n", w.Profile)
+
+	// Okay, now let's render some markdown.
+	r, err := glamour.NewTermRenderer(glamour.WithEnvironmentConfig())
+	if err != nil {
+		log.Fatal(err)
+	}
+	md, err := r.RenderBytes(buf.Bytes())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	r, err := glamour.NewTermRenderer(
-		glamour.WithStandardStyle(style),
-		glamour.WithColorProfile(termenv.TrueColor),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	md, err := r.RenderBytes(b)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Fprintf(os.Stdout, "%s\n", md)
+	// And finally, write it to stdout using the colorprofile writer. This will
+	// ensure colors are downsampled if necessary.
+	fmt.Fprintf(w, "%s\n", md)
 }
---
+++ examples/go.mod
@@ -0,0 +1,34 @@
+module examples
+
+go 1.24.0
+
+require (
+	github.com/charmbracelet/colorprofile v0.2.1
+	github.com/charmbracelet/glamour v0.8.0
+)
+
+require (
+	github.com/alecthomas/chroma/v2 v2.14.0 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/charmbracelet/lipgloss v1.0.0 // indirect
+	github.com/charmbracelet/x/ansi v0.8.0 // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/dlclark/regexp2 v1.11.0 // indirect
+	github.com/gorilla/css v1.0.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.27 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.3-0.20240618155329-98d742f6907a // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	github.com/yuin/goldmark v1.7.8 // indirect
+	github.com/yuin/goldmark-emoji v1.0.4 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/term v0.29.0 // indirect
+)
+
+replace github.com/charmbracelet/glamour => ../
